e011acbfc1bf75f195b10551b4730a6b6063f095
Edward Irby <e.irby@pm.me>
2023-11-10 13:28:42 -0800

    feat: eslint-prettier-config

---
+++ .eslintrc.cjs
@@ -0,0 +1,31 @@
+module.exports = {
+  ignorePatterns: ['**/dist/*'],
+  env: {
+    browser: true,
+    es2022: true,
+    node: true,
+  },
+  extends: ['eslint:recommended', 'plugin:compat/recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    allowImportExportEverywhere: true,
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-uses-react': 'off',
+    'react/prop-types': 'off',
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
+    '@typescript-eslint/ban-ts-comment': [
+      2,
+      {
+        'ts-ignore': 'allow-with-description',
+      },
+    ],
+  },
+  settings: {
+    'compat/compat': { browserslist: ['> 2%'] },
+  },
+}
---
+++ README.md
@@ -0,0 +1,21 @@
+# Plaited eslint config preset
+
+An opinionated Eslint config used for Plaited repositories.
+
+## Installation
+
+Install eslint and this config:
+
+```
+npm i -D eslint @plaited/eslint-config
+```
+
+Now in your `package.json`:
+
+```
+{
+  "eslintConfig": {
+    "extends": "@plaited/eslint-config"
+  }
+}
+```
